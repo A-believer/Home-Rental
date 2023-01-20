de4b8646c9298cdaa27425c37d3949c2a233b507
davedev <davidabolade29@gmail.com>
2023-01-20 15:14:05 +0100

    halfway through setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-pink text-blue-800 first-letter:text-yellow">
-     Hello
+    <div className="text-blue-400 bg-orange ">
+      Hello
+      <h1 className='text-darkBlue'>World</h1>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,31 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        primary: "#18191F",
+        secondary: "#252B42",
+        orange: "#F4511E",
+        grey: "#818181",
+        gray: "#BDBDBD",
+        darkBlue: "#0A142F",
+        offWhite: "#F5F5F5"
+      },
+      backgroundImage: {
+        gradient1: "linear-gradient(200.44deg, rgba(0, 0, 0, 0.0001) 16.41%, rgba(0, 0, 0, 0.51) 77.16%)",
+        gradient2: "linear-gradient(180deg, rgba(0, 0, 0, 0) 14.58%, rgba(56, 56, 56, 0.84) 100%)",
+        hbg: "url('/assets/images/headerbg.png')"
+      }
+
+
+    },
   },
   plugins: [],
 }
